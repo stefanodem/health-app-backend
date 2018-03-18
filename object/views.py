2e2b7fca710eb3815bf4ac4230ef4129dca0e6da
steve841 <stdemicheli@gmail.com>
2018-03-18 16:05:36 +0100

    Added delete_like

--- object/views.py
+++ object/views.py
@@ -8,12 +8,17 @@ from like.models import Like
 object_app = Blueprint('object', __name__)
 
 
-@object_app.route('/users/<int:user_id>/posts/<int:post_id>/like', methods=['POST'])
-def add_like(user_id, post_id):
+@object_app.route('/users/<int:user_id>/posts/<int:post_id>/like', methods=['POST', 'DELETE'])
+def handle_like(user_id, post_id):
     if request.is_json and request.method == 'POST':
-        liked_post = Like(user_id=user_id, object_id=post_id)
-        db.session.add(liked_post)
+        post_like = Like(user_id=user_id, object_id=post_id)
+        db.session.add(post_like)
         db.session.commit()
         return json.dumps({'success': True}), 201, {'ContentType': 'application/json'}
+    if request.is_json and request.method == 'DELETE':
+        post_like = Like.query.filter_by(user_id=user_id, object_id=post_id).one()
+        db.session.delete(post_like)
+        db.session.commit()
+        return json.dumps({'success': True}), 204, {'ContentType': 'application/json'}
     else:
         abort(400)
